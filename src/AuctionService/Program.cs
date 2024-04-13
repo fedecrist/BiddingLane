95ff9b88350b7faa71a6eb776a6815432d5528be
Federico Cristofanelli <fedecristofanelli@outlook.es>
2024-04-13 15:36:18 -0300

    Initial commit

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,14 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
